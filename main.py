30ba89044ba2c3216528286c616eda806855318f
tsepanx <77626319+tsepanx@users.noreply.github.com>
2022-10-27 23:53:10 +0300

    working

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("YOUR TOKEN HERE").build()
-
+app = ApplicationBuilder().token("5569023082:AAGhXT_-8V709h4XwFmCy8KN0EaFRmpnlE0").build()
 app.add_handler(CommandHandler("hello", hello))
 
 app.run_polling()
