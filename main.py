4741fafae3e0d5870dd3550161f724d3d544ddf6
tsepanx <77626319+tsepanx@users.noreply.github.com>
2022-10-27 23:56:48 +0300

    add .token read

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 
-app = ApplicationBuilder().token("5569023082:AAGhXT_-8V709h4XwFmCy8KN0EaFRmpnlE0").build()
+TOKEN = open('.token').read()
+print(TOKEN)
+
+app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("hello", hello))
 
 app.run_polling()
