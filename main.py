b3906fb341b0dd5c33e57da5af5b190fa3c965d3
tsepanx <77626319+tsepanx@users.noreply.github.com>
2022-11-16 17:42:58 +0300

    Add only new entries to msg, separate feeds entries

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
+import dataclasses
 import datetime
+from typing import Optional
 
 from telegram import Update
 from telegram.constants import ParseMode
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-from telegram.ext.filters import BaseFilter
 
 from functools import wraps
-
 from rss import get_parsed_feed
 
-data_dict: dict[int, list] = {}
+
+@dataclasses.dataclass
+class FeedDataclass:
+    url: str
+    last_update_time: Optional[datetime.datetime] = datetime.datetime.min
+    # last_item_id: Optional[str] = None
+
+    def __str__(self):
+        return self.url
+
+
+data_dict: dict[int, list[FeedDataclass]] = {}
 
 
 def handler_decorator(func):
@@ -32,7 +43,9 @@ async def add_command(update: Update, _):
     print(update.effective_user.id)
     try:
         rss_link = update.message.text.split(' ')[1]
-        data_dict[update.effective_user.id].append(rss_link)
+        data_dict[update.effective_user.id].append(
+            FeedDataclass(rss_link)
+        )
 
         await update.message.reply_text(f"Subscribed {rss_link}")
     except Exception:
@@ -42,53 +55,75 @@ async def add_command(update: Update, _):
 @handler_decorator
 async def list_command(update: Update, _):
     user_feeds = data_dict[update.effective_user.id]
-    await update.message.reply_text('List:\n' + '\n'.join(user_feeds))
+    await update.message.reply_text('List:\n' + '\n'.join(map(str, user_feeds)))
 
 
 @handler_decorator
 async def del_command(update: Update, _):
+    current_chat_feeds = data_dict[update.effective_user.id]
     try:
         rss_link = update.message.text.split(' ')[1]
-        data_dict[update.effective_user.id].remove(rss_link)
+        feed_dataclass_obj = list(filter(lambda x: x.url == rss_link, current_chat_feeds))[0]
+        current_chat_feeds.remove(feed_dataclass_obj)
         await update.message.reply_text(f"Removed {rss_link}")
     except Exception:
         await update.message.reply_text(f"Error removing")
 
 
 @handler_decorator
-async def plain_text(update: Update, _):
+async def plaintext_handler(update: Update, _):
     text = update.message.text
     await update.message.reply_text(text, parse_mode=ParseMode.MARKDOWN)
 
 
 @handler_decorator
 async def fetch_command(update: Update, _):
-    entries_list = []
-
-    feeds_urls = data_dict[update.effective_user.id]
-    for url in feeds_urls:
-        feed_obj = get_parsed_feed(url)
-        parsed_entries = feed_obj.entries
-        entries_list.extend(parsed_entries)
-
-    entries_list.sort(key=lambda x: datetime.datetime.fromisoformat(x.published), reverse=True)
-
+    current_chat_feeds: list[FeedDataclass] = data_dict[update.effective_user.id]
     result_msg_str = ''
 
-    for i in range(len(entries_list)):
-        entry = entries_list[i]
-        entry_id = entry.id
-        entry_link = entry.link
-        entry_published = datetime.datetime.fromisoformat(entry.published)
-        entry_title = entry.title
-        entry_content = entry.content
+    for feed_obj in current_chat_feeds:
+        parser_obj = get_parsed_feed(feed_obj.url)
 
-        result_msg_str += f"[{i}]({entry_link}) {entry_title}\n"
+        result_msg_str += f'**{parser_obj.feed.title}**\n\n'
+
+        entries_list = parser_obj.entries
+        entries_list.sort(key=lambda x: datetime.datetime.fromisoformat(x.published), reverse=True)
+
+        # Take only entries, published after last update time
+        entries_list = list(filter(
+            lambda x: datetime.datetime.fromisoformat(x.published).replace(tzinfo=None) > feed_obj.last_update_time,
+            entries_list
+        ))
+
+        if not entries_list:
+            result_msg_str += 'EMPTY\n'
+            continue
+
+        for i in range(len(entries_list)):
+            entry = entries_list[i]
+            entry_id = entry.id
+            entry_link = entry.link
+            entry_published = datetime.datetime.fromisoformat(entry.published)
+            entry_title = entry.title
+            entry_content = entry.content
+
+            result_msg_str += f"[{i}]({entry_link}) {entry_title}\n"
+
+        feed_obj.last_update_time = datetime.datetime.now()
+        result_msg_str += "\n"
 
     await update.message.reply_text(
         result_msg_str,
         parse_mode=ParseMode.MARKDOWN
     )
+
+
+async def callback_every_second(context: ContextTypes.DEFAULT_TYPE):
+    for i in data_dict.keys():
+        await context.bot.send_message(
+            chat_id=i,
+            text='One message every 1 second'
+        )
 
 
 TOKEN = open('.token').read()
@@ -102,22 +137,10 @@ commands_funcs_mapping = {
 }
 
 app = ApplicationBuilder().token(TOKEN).build()
-
-async def callback_second(context: ContextTypes.DEFAULT_TYPE):
-    for i in data_dict.keys():
-        await context.bot.send_message(
-            chat_id=i,
-            text='One message every 1 second'
-        )
-
-jq = app.job_queue
-job_minute = jq.run_repeating(callback_second, interval=1, first=0)
-
+# job_minute = app.job_queue.run_repeating(callback_every_second, interval=1, first=0)
 
 for command_string, func in commands_funcs_mapping.items():
-    app.add_handler(
-        CommandHandler(command_string, func)
-    )
+    app.add_handler(CommandHandler(command_string, func))
 
-app.add_handler(MessageHandler(filters.TEXT, plain_text))
+app.add_handler(MessageHandler(filters.TEXT, plaintext_handler))
 app.run_polling()
--- rss.py
+++ rss.py
@@ -1,9 +1,5 @@
-import datetime
-import time
-
 import feedparser
 
 
 def get_parsed_feed(url: str) -> feedparser.FeedParserDict:
     return feedparser.parse(url)
-    # return parsed.entries
